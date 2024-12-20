fb65bb5ccde34ce2eeb7f3ab59211dc22c9a7904
Sana Khemir <archi.khemirsana@gmail.com>
2024-12-20 18:32:19 +0100

    changement

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
 function insertionSort(arr) {
     for (let i = 1; i < arr.length; i++) {
-      let key = arr[i]; // L'élément à insérer dans la séquence triée
+      let key = arr[i]; 
       let j = i - 1;
-  
-      // Déplacez les éléments de arr[0..i-1] qui sont plus grands que key
-      // d'une position vers la droite
+
       while (j >= 0 && arr[j] > key) {
         arr[j + 1] = arr[j];
         j--;
       }
   
-      // Insérez key à sa position correcte
       arr[j + 1] = key;
     }
   
-    return arr; // Retourne le tableau trié
+    return arr; 
   }
   
   // Exemple d'utilisation
